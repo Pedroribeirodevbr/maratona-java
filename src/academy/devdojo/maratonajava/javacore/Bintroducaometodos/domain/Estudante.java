bd8e1e9259cfe55b510b020a234c547df6efa658
Pedroribeirodevbr <gestorpedroribeiro@gmail.com>
2026-06-28 21:20:33 -0300

    Aula de Metodos usando parâmetros tipo referência

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Estudante.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
+
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest04.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.Calculadora;
+
+public class CalculadoraTest04 {
+    static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int num1 = 1;
+        int num2 = 2;
+        calculadora.alteraDoisNumeros(num1, num2);
+        System.out.println("Dentro do CalculadoraTest04");
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
